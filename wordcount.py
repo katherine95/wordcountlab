c36c5b68e3574b0582a7410717759a0b916995ed
katherine95 <kathiekim95@gmail.com>
2017-03-08 16:16:13 +0300

    changes

--- wordcount.py
+++ wordcount.py
@@ -1,14 +1,14 @@
-def words(a):
-  '''count number of times a word occurs in a string'''
-  #assigns every word(key) in the dictionary with a value 
-	count=dict()
-	#splits the words in the string a and stores in the variable words
-	words=a.split()
-	#for x(every word) in words
-	for x in words:
-	  #counts x and gives value(number of times occured(+1)) if there is no word gives none
-		count[x]=count.get(x, 0)+1
-		#return every word(x) with the value
-	return count
-#a is the string given
-a=("olly olly in come free in my in dog")	
+#input string
+def words(string):
+  Dict = {}#change string to dictionary
+  strsplit = string.split()#split dictionary to individual keys/word
+  if len(strsplit) == 1:#if length of the word in the dictionary is one return the word
+    Dict[strsplit[0]] = 1
+    return Dict
+  else:
+    for word in strsplit:
+      if word.isdigit():#if word in the dictionary is an integer return number 
+        word = int(word)
+      Dict[word] = strsplit.count(str(word))#change input(number) to string
+    return Dict  
+
